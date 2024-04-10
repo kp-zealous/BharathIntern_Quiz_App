eb767d6e08dc87bf1bc4832ae9a8efbe10a65e97
sowmiya-msec20 <150236600+sowmiya-msec20@users.noreply.github.com>
2024-04-10 21:02:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What does AI stand for?",
+            "Which programming language is commonly used in AI development?",
+            "What is the name of the AI technology that allows machines to perform tasks that typically require human intelligence?",
+            "Who is considered the father of artificial intelligence?",
+            "What is the name of the AI system developed by IBM that defeated human champions in the game show Jeopardy?",
+            "What is the primary goal of machine learning?",
+            "What is the name of the process in which an AI system improves its performance over time through experience?",
+            "Which company developed the widely-used open-source machine learning library TensorFlow?",
+            "What is the name of the AI assistant developed by Apple?",
+            "What is the term used to describe the ability of AI systems to understand and interpret human language?"
+    };
+
+    public static String choices [][]={
+            {"Artificial Intelligence" , "Advanced Interface" , "Automated Interaction" , "Advanced Intelligence"},
+            {"Python" , "Java" , "C++" , "Ruby"},
+            {"Machine Learning" , "Deep Learning" , "Natural Language Processing" , "Reinforcement Learning"},
+            {"Alan Turing" , "John McCarthy" , "Marvin Minsky" , "Herbert Simon"},
+            {"Watson" , "DeepMind" , "AlphaGo" , "Cerebras"},
+            {"To learn from data and make predictions or decisions" , "To replace human intelligence with machine intelligence" , "To create human-like robots" , "To control the world"},
+            {"Deep Learning" , "Machine Learning" , "Reinforcement Learning" , "Natural Language Processing"},
+            {"Google" , "Microsoft" , "Facebook" , "Apple"},
+            {"Siri" , "Alexa" , "Cortana" , "Google Assistant"},
+            {"Natural Language Understanding" , "Speech Recognition" , "Computer Vision" , "Natural Language Processing"}
+    };
+
+    public static String correctAnswers []={
+            "Artificial Intelligence" ,
+            "Python",
+            "Machine Learning",
+            "John McCarthy",
+            "Watson",
+            "To learn from data and make predictions or decisions",
+            "Machine Learning",
+            "Google",
+            "Siri",
+            "Natural Language Processing"
+    };
+}
